d3f93986e5d8d71be64b852bdcaddc8ccfda8181
kiptoo-097 <kiptoo097.com>
2020-11-04 14:13:31 +0300

    Modified parent class confiquration

--- config.py
+++ config.py
@@ -1,6 +1,40 @@
 import os
 
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+    API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://moringa:Kamunguna1@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+
+   
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+    
+
+
+class DevConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://moringa:Kamunguna1@localhost/blogs'
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    
+    DEBUG = True
+    
+config_options = {'development': DevConfig,'production': ProdConfig}
