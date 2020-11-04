cc62da89a1d5b016f1aafa0d55859d5fea209b58
kiptoo-097 <kiptoo097.com>
2020-11-04 14:14:18 +0300

    Added email configurations

--- config.py
+++ config.py
@@ -10,7 +10,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
-   
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
