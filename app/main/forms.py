7b0866eaf0901e032468ba7cfcc03586ccf03415
kiptoo-097 <kiptoo097.com>
2020-11-04 14:22:41 +0300

    Modified login and Register form

---
+++ app/main/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required, DataRequired
+
+
+class BlogForm(FlaskForm):
+    category = SelectField(
+        "Select Category",
+        choices=[
+            ("product", "Product"),
+            ("interview", "Interview"),
+            ("promotion", "Promotion"),
+        ],
+    )
+    content = TextAreaField("Your Blog")
+    submit = SubmitField("Create Blog")
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Comment")
+    submit = SubmitField("Submit")
+
+
+
