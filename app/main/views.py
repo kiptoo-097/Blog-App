08772c5318d040d17f452ba9c419e8c462f7003a
kiptoo-097 <kiptoo097.com>
2020-11-04 13:58:36 +0300

    Added requests forms, models to imports

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,9 @@
 from flask import render_template, redirect, url_for, request
-
+from flask_login import login_required, current_user
+from . import main
+from ..models import Blog, User, Comment
+from .forms import BlogForm, CommentForm
+from .. import db
+from ..requests import get_quotes
 
 
